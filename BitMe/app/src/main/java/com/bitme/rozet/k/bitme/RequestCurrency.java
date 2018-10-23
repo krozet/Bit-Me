e094ec6a8c2ea47c0341d261ef3f37d7033b6d6c
Keawa Rozet <keawa.rozet@yahoo.com>
2018-10-22 21:11:43 -0700

    KR added welcome message in the form of an imageview.

---
+++ BitMe/app/src/main/java/com/bitme/rozet/k/bitme/RequestCurrency.java
@@ -0,0 +1,13 @@
+package com.bitme.rozet.k.bitme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RequestCurrency extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_currency);
+    }
+}
