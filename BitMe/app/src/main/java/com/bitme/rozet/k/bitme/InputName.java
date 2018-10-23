d6517745bb45c28c7cc5d362e12439b1667367a1
krozet <keawa.rozet@yahoo.com>
2018-10-23 14:05:35 -0700

    KR name validation.

--- BitMe/app/src/main/java/com/bitme/rozet/k/bitme/InputName.java
+++ BitMe/app/src/main/java/com/bitme/rozet/k/bitme/InputName.java
@@ -1,13 +1,54 @@
 package com.bitme.rozet.k.bitme;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InputName extends AppCompatActivity {
+    EditText userNameEditText;
+    Button submitButton;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_name);
+        
+        setupUserNameEditText();
+        setupSubmitButton();
+        
+    }
+
+    private void setupSubmitButton() {
+        submitButton = findViewById(R.id.inputname_submit);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validateName()) {
+                    Toast.makeText(getApplicationContext(), name,
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please enter a valid name.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    private boolean validateName() {
+        name = userNameEditText.getText().toString();
+        // checks if name is empty and if it only contains valid chars
+        return !name.isEmpty() && name.matches("[a-zA-z]+([ '-][a-zA-Z]+)*");
+    }
+
+    private void setupUserNameEditText() {
+        userNameEditText = findViewById(R.id.inputname_enter_name_field);
+        userNameEditText.setTextColor(Color.parseColor("#FF9800"));
+        userNameEditText.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/mentone_semibol_ita.otf"));
     }
 }
--- BitMe/app/src/main/java/com/bitme/rozet/k/bitme/RequestCurrency.java
+++ BitMe/app/src/main/java/com/bitme/rozet/k/bitme/RequestCurrency.java
@@ -1,13 +1,52 @@
 package com.bitme.rozet.k.bitme;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RequestCurrency extends AppCompatActivity {
+    private static final int RESULT_FIRST_START = 1;
+
+    SharedPreferences sharedPreferences;
+    String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_currency);
+
+        checkPreviouslyStarted();
+        name = sharedPreferences.getString("userName", "User");
+    }
+
+    private void checkPreviouslyStarted() {
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean firstStart = sharedPreferences.getBoolean("isFirstStart", true);
+        if(firstStart) {
+            openInputNameActivity();
+        }
+    }
+
+    private void openInputNameActivity() {
+        Intent intent = new Intent(this, InputName.class);
+        startActivityForResult(intent, RESULT_FIRST_START);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(resultCode) {
+            case RESULT_FIRST_START:
+                if (resultCode == RESULT_OK) {
+                    SharedPreferences.Editor edit = sharedPreferences.edit();
+                    edit.putBoolean("isFirstStart", Boolean.FALSE);
+                    edit.apply();
+                }
+                break;
+        }
     }
 }
