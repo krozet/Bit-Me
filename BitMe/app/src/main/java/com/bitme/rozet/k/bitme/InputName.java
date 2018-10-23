14ebfa8738cfc4bf4a231190469fb7aee1ff3cff
Keawa Rozet <keawa.rozet@yahoo.com>
2018-10-22 17:04:36 -0700

    KR init android studio project.

---
+++ BitMe/app/src/main/java/com/bitme/rozet/k/bitme/InputName.java
@@ -0,0 +1,13 @@
+package com.bitme.rozet.k.bitme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InputName extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_name);
+    }
+}
