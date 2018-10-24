16174b907bd0b845270e06dd1373e1cbf2e2b172
Keawa Rozet <keawa.rozet@yahoo.com>
2018-10-23 19:41:21 -0700

    KR setup all text views and button.

--- BitMe/app/src/main/java/com/bitme/rozet/k/bitme/CurrencyConversion.java
+++ BitMe/app/src/main/java/com/bitme/rozet/k/bitme/CurrencyConversion.java
@@ -4,9 +4,14 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class CurrencyConversion extends AppCompatActivity {
+    TextView numberTextView, abrvTextView, oneBTCTextView, todaysValueTextView, equivalentTextView;
+    Button returnButton;
+
     SharedPreferences sharedPreferences;
     Currencies currencies;
 
@@ -19,6 +24,36 @@ public class CurrencyConversion extends AppCompatActivity {
         setContentView(R.layout.activity_currency_conversion);
 
         init();
+        setupNumberTextView();
+        setupAbrvTextView();
+        setupOneBTCTextView();
+        setupTodaysValueTextView();
+        setupEquivalentTextView();
+        setupReturnButton();
+    }
+
+    private void setupReturnButton() {
+        returnButton = findViewById(R.id.currencyconversion_return);
+    }
+
+    private void setupEquivalentTextView() {
+        equivalentTextView = findViewById(R.id.currencyconversion_equivalent_text);
+    }
+
+    private void setupTodaysValueTextView() {
+        todaysValueTextView = findViewById(R.id.currencyconversion_todays_value_text);
+    }
+
+    private void setupOneBTCTextView() {
+        oneBTCTextView = findViewById(R.id.currencyconversion_one_btc_text);
+    }
+
+    private void setupAbrvTextView() {
+        abrvTextView = findViewById(R.id.currencyconversion_abrv);
+    }
+
+    private void setupNumberTextView() {
+        numberTextView = findViewById(R.id.currencyconversion_number);
     }
 
     private void init() {
